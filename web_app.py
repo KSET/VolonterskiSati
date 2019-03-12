abbdff8a69cda00cc87fdacc3fc73d265e5a5430
Mladen Petr <mladenpetr94@gmail.com>
2019-03-12 18:43:50 +0100

    Added simple html templates

--- web_app.py
+++ web_app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from DatabaseController import DatabaseController
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return "<html><body><h1>'Hello World'</h1></body></html>"
+    db = DatabaseController()
+    start_date = datetime.datetime(2019, 2, 1)
+    end_date = datetime.datetime(2019, 3, 15)
+    activity = db.get_period_activity(start_date, end_date)
+    total_activity = {}
+    for item in activity:
+        key = "%s %s" % (item[0], item[1])
+        if key not in total_activity:
+            total_activity[key] = 0
+        total_activity[key] += item[2] * item[3]
+    return render_template("index.html", list_records = {})
 
 if __name__ == '__main__':
    app.run(debug = True)
